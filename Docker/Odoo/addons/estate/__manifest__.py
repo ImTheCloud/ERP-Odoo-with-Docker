4175f60dbe206d71904dd806736741c0beea9eb3
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2023-12-04 12:21:03 +0100

    TP 1 FINI COMPLET vient de DISK

--- Docker/Odoo/addons/estate/__manifest__.py
+++ Docker/Odoo/addons/estate/__manifest__.py
@@ -1,10 +1,12 @@
 # coding: utf-8 
 {
-
- 'name': "estate",
- 'author': "Popadiuc Claudiu, Redouan Touri",
- 'depends': ['base'],
- 'application': True,
-
-
+    'name': "estate",
+    'author': "Popadiuc Claudiu, Redouan Touri",
+    'depends': ['base'],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'application': True,
 }
