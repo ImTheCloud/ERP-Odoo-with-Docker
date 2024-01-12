ca1a99d57668e404d77599d0bf359ef2cd3fd7c1
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2024-01-12 16:11:19 +0100

    Version 1.1
    New estate with field Employee and date

--- Docker/Odoo/addons/estate/__manifest__.py
+++ Docker/Odoo/addons/estate/__manifest__.py
@@ -1,12 +1,9 @@
-# coding: utf-8 
 {
     'name': "estate",
     'author': "Popadiuc Claudiu, Redouan Touri",
-    'depends': ['base'],
-    'data':[
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml',
+    'category': 'Uncategorized',
+    'depends': ['base', 'sale_management', 'calendar', 'hr'],
+    'data': [
+        'views/sale_order.xml',
     ],
-    'application': True,
 }
